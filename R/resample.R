0cd64639ca88c522a836bc52d93891059445890e
thomaz <thomazbastiaansser@gmail.com>
2022-12-22 18:36:03 +0000

    edit

--- R/resample.R
+++ R/resample.R
@@ -15,14 +15,42 @@ sampleBetaBinom <- function(samples, k, n){
 #' samples from the log() of the geometric mean.
 #'
 #' @param samples An integer. How many samples should be taken
-#' @param count_samples A vector of count data.
+#' @param count_sample A vector of count data.
 #' 
 #' @export
 #' 
-sampleGeomMeam <- function(samples, count_sample){
+sampleGeomMeam <- function(samples, count_sample, log_transformed = T){
   #Make a matrix with resamples for each feature.
-  sample_list = sapply(count_sample, FUN = function(x){sampleBetaBinom(samples = samples, k = x, n = sum(data))}, simplify = T)
+  sample_mat = sampleEachBetaBinom(samples = samples, count_sample = count_sample)
   
   #Take the average of the log-transformed values, which corresponds to the log-transformed geometric mean.
-  apply(X = log(sample_list), MARGIN = 1, FUN = mean)
+  geom_mean = apply(X = log(sample_mat), MARGIN = 1, FUN = mean)
+  
+  if(!log_transformed){geom_mean = exp(geom_mean)}
+  
+  return(geom_mean)
+}
+
+#' samples from the probability density function of a feature in count data for each feature.  
+#'
+#' @param samples An integer. How many samples should be taken
+#' @param count_sample A vector of count data.
+#' 
+#' @export
+#' 
+sampleEachBetaBinom <- function(samples, count_sample){
+  return(sapply(count_sample, FUN = function(x){sampleBetaBinom(samples = samples, k = x, n = sum(count_sample))}, simplify = T))
+}
+
+#' samples from the probability density function of a CLR-transformed count sample.  
+#'
+#' @param samples An integer. How many samples should be taken
+#' @param count_sample A vector of count data.
+#' 
+#' @export
+#' 
+sampleCLR <- function(samples, count_sample){
+  log_ratios = log(sampleEachBetaBinom(samples = samples, count_sample = count_sample)) - sampleGeomMeam(samples = samples, count_sample = count_sample, log_transformed = T)
+  
+  return(log_ratios)
 }
